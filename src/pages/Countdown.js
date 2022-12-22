13112b586eba88be2ff754fbcb6efcd921643493
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2022-12-22 01:54:13 +0100

    i worked on the coming soon page

--- src/pages/Countdown.js
+++ src/pages/Countdown.js
@@ -15,6 +15,7 @@ const Countdown = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(email)
+        document.getElementById("user-success").style.display = "block"
     }
 
     return (
@@ -24,44 +25,35 @@ const Countdown = () => {
                     <div className="nav">
                         <img className="logo2" src="images/logo-2.png" alt="" />
                     </div>
-                    <h2 className="launch">We are Launching soon! <img className="launch-img" src="images/launch.gif" alt="" /></h2>
-                    <br />
+
                     <div className="row">
-                        <div className="col-lg-6">
+                        <div className="col-lg-6 notifications">
                             <h1 className="countdown-header"><div className="get-notified">Get Notified</div>
                                 When we Launch.</h1>
                             <p className="countdown-p1">Our website launches soon. In the meantime, <br />stay with us.</p>
-                            <div className="row">
-                                <div className="col-lg-6">
-                                    <p style={{ fontSize: "12px", letterSpacing: "0.08rem" }}>DAYS |  HOURS  | MINUTES |  SECONDS</p>
-                                    <h3 style={{ fontWeight: "bold", letterSpacing: "0.06rem" }}>12 : 10 : 46 : 13</h3>
-                                </div>
-                                <div className="col-lg-6">
-                                    <p className="days">12 days left</p>
-                                </div>
-                            </div>
-                            <br />
-                            <form onSubmit={handleSubmit}>
+                            <form className="form" onSubmit={handleSubmit}>
                                 <input
                                     type="email"
                                     className="countdown-input"
+                                    placeholder="Enter your email address"
                                     onChange={handleChange}
                                 />
                                 <button className="notify-button">Notify me</button>
+                                <p id="user-success" className="user-success">You will be notified when we launch!</p>
                             </form>
+                            <div className="footer-countdown" style={{ color: "grey", fontSize: "30px" }}>
+                                <span><i class="fa-brands fa-whatsapp"></i> <i class="fa-brands fa-instagram" style={{ marginLeft: "30px" }}></i> <i class="fa-brands fa-twitter" style={{ marginLeft: "30px" }}></i></span>
+                            </div>
                         </div>
                         <div className="col-lg-6">
-                            <img className="countdown-img" src="images/countdown-img.png" alt="" />
+                            <img className="isend-launch img-fluid" src="images/isend-launch.gif" alt="isend-launch" />
+                            <div className="countdown-rec">
+                            </div>
                         </div>
                     </div>
                 </div>
             </section>
             {/* ---------------------------------------------------------------------------------------------- */}
-            <section id="footer-countdown">
-                <div className="footer-countdown" style={{ textAlign: "center", color: "grey", fontSize: "30px" }}>
-                    <span><i class="fa-brands fa-facebook"></i> <i class="fa-brands fa-instagram" style={{ marginLeft: "30px" }}></i> <i class="fa-brands fa-twitter" style={{ marginLeft: "30px" }}></i></span>
-                </div>
-            </section>
         </>
     )
 }
