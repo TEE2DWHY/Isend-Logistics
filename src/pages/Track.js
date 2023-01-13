f2455257d19046ca32cb113c3b768832b7857fae
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-01-13 13:36:13 +0100

    started working on the order-overview page

--- src/pages/Track.js
+++ src/pages/Track.js
@@ -32,7 +32,7 @@ const Track = () => {
                         placeholder="Enter your Tracking ID"
                         style={{ padding: "10px 0px 10px 20px", width: "50%", border: "none" }}
                     />
-                    <button style={{ height: "30px", border: "none", height: "44px", backgroundColor: "#1B1B1B", color: "#fff", borderRadius: "0px 6px 6px 0px", padding: "0 20px" }}>Search</button>
+                    <button style={{ border: "none", height: "44px", backgroundColor: "#1B1B1B", color: "#fff", borderRadius: "0px 6px 6px 0px", padding: "0 20px" }}>Search</button>
                 </form>
                 <br /> <br />
                 <p style={{ color: "#F35A5A", fontStyle: "italic", paddingBottom: "8%" }}>* This tracking id does not exist</p>
