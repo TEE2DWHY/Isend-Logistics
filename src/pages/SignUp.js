edc0ddb8124e73ab46018dd76cf747d65ced14f8
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-01-19 14:51:57 +0100

    made new changes to the form pages

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -54,6 +54,7 @@ const SignUp = () => {
                         <p style={{ textAlign: "center", fontSize: "14px", marginTop: "10px" }}>Already have an account? <Link to="/login"><b>Sign in </b></Link></p>
                     </form>
                     <br />
+                    <p>Powered by <img src="images/logo.png" alt="logo" style={{width:"50px", height:"14.82px", marginLeft:"3px"}}/></p>
                     <p>You agree to iSend’s <Link to="/privacy-policy"><span style={{ color: "#F2C040", lineHeight: "1.2" }}>Terms of Use & Privacy Policy</span></Link>. You don't need to consent as a condition of renting any property, or buying any other goods or services. Message/data rates may apply.</p>
                 </div>
             </section>
