bac8ab938a2cdeae868ff8c395cc2a3299ea68c4
TEE2DWHY <88804937+TEE2DWHY@users.noreply.github.com>
2023-01-21 21:44:35 +0100

    cretaed the user auth page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Blog from "./pages/Blog";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Privacy from "./pages/Privacy";
+import Auth from "./pages/Auth";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Terms from "./pages/Terms";
@@ -46,6 +47,7 @@ function App() {
           <Route path="/blog" element={<Blog />} />
           <Route path="/privacy-policy" element={<Privacy />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/auth" element={<Auth/>}/>
           <Route path="/login" element={<Login />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/terms-of-use" element={<Terms />} />
